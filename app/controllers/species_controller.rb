02822b6417a20ea6a822c35fd8276da68795bab1
J.J. Idt & Austin Steward <student@epicodus.com>
2014-08-20 10:01:46 -0700

    add new species form page

--- app/controllers/species_controller.rb
+++ app/controllers/species_controller.rb
@@ -1,6 +1,11 @@
 class SpeciesController < ApplicationController
   def index
-    @species = Species.all
+    @specieses = Species.all
     render('species/index.html.erb')
+  end
+
+  def new
+    @species = Species.new
+    render('species/new.html.erb')
   end
 end
